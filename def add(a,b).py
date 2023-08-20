bbd06eb6d1476e06753c336cd284bfb87be2e791
변경환 <byunkyunghwan@byeongyeonghwan-ui-MacBookPro.local>
2023-08-20 23:59:22 +0900

    add function

---
+++ def add(a,b).py
@@ -0,0 +1,15 @@
+#기본 계산기
+def add(a,b):
+	return a+b
+
+def subtract(a, b):
+	return a - b
+
+def multiply(a, b):
+	return a * b
+
+def divide_premium(a, b):
+	return a / b
+
+def divide_main(a, b):
+	return a/b
